9b5e40ce53d27d772af7fb1d63523cdf411dcabf
leeky <ky2710@gmail.com>
2022-08-18 18:01:52 +0900

    fetch movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-	return <div className="App"></div>;
+	const [loading, setLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
+			.then((response) => response.json())
+			.then((obj) => {
+				setMovies(obj.data.movies);
+				setLoading(false);
+				console.log(movies);
+			});
+	}, []);
+	console.log('rendering.....');
+	return (
+		<div className="App">
+			<h1>Movie Appp</h1>
+
+			{loading ? <h2>Movie Lists loading.....</h2> : null}
+		</div>
+	);
 }
 
 export default App;
