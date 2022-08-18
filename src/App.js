9e3bfc0a8a3b412dd04e8806c03ebb83666cef69
leeky <ky2710@gmail.com>
2022-08-18 18:31:36 +0900

    loading movies & rendering Movie List

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import { useState, useEffect } from 'react';
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [movies, setMovies] = useState([]);
-
+	const getMovies = async () => {
+		const response = await fetch(
+			'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+		);
+		const obj = await response.json();
+		setMovies(obj.data.movies);
+		setLoading(false);
+		console.log('data : ', obj.data.movies);
+	};
 	useEffect(() => {
-		fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
-			.then((response) => response.json())
-			.then((obj) => {
-				setMovies(obj.data.movies);
-				setLoading(false);
-				console.log(movies);
-			});
+		getMovies();
 	}, []);
+
 	console.log('rendering.....');
 	return (
 		<div className="App">
 			<h1>Movie Appp</h1>
 
-			{loading ? <h2>Movie Lists loading.....</h2> : null}
+			{loading ? (
+				<h2>Movie Lists loading.....</h2>
+			) : (
+				movies.map((movie) => <div key={movie.id}>{movie.title}</div>)
+			)}
 		</div>
 	);
 }
